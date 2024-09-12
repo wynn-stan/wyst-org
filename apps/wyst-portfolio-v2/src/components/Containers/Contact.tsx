8da7988002ce53122b78cecc644945a20cb5ac34
wynn-stan <winston@inpathgroup.africa>
2024-09-12 20:13:20 +0000

    update link

--- apps/wyst-portfolio-v2/src/components/Containers/Contact.tsx
+++ apps/wyst-portfolio-v2/src/components/Containers/Contact.tsx
@@ -47,7 +47,7 @@ export default function Contact() {
             <div>Email</div>
           </ContactItem>
         </div>
-        <ContactItem href="https://drive.google.com/file/d/1_cRmAl3xuZ4xMUqp3k6Och1KoRvKxu3S/view?usp=sharing">
+        <ContactItem href="https://drive.google.com/file/d/1H1N131Z7ly26-o2tOR8KpB9FoL3QLUTA/view?usp=sharing">
           <ScrollTextIcon size={24} />
           <div>Resume</div>
         </ContactItem>
